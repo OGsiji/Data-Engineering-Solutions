36d3e543263b2258ffa07c0db78110a7de35162b
OGsiji <67172880+OGsiji@users.noreply.github.com>
2021-10-15 01:45:08 +0200

    Create Question 3 Postgress pipeline by writing.sql

---
+++ Question 3 Postgress pipeline by writing.sql
@@ -0,0 +1,10 @@
+Question 3
+
+
+
+#Lets Assume the Table name is Event
+#the following statement returns the id of the inserted row
+
+INSERT INTO Event(operationType)
+VALUES (value1, value2)
+RETURNING _id;
