c85b997657695ba7ea3eb46e98ac1b9cf5d0ad71
OGsiji <67172880+OGsiji@users.noreply.github.com>
2021-10-15 01:59:27 +0200

    Update Question 3 Postgress pipeline by writing.sql

--- Question 3 Postgress pipeline by writing.sql
+++ Question 3 Postgress pipeline by writing.sql
@@ -2,8 +2,9 @@ Question 3
 
 
 
-#Lets Assume the Table name is Event
-#the following Postgress RDBMS solution  returns the id of the inserted row
+''' -Lets Assume the Table name is Event
+    -the following Postgress RDBMS solution returns the id of the inserted row
+    -we are inserting value 1 into first row and value 2 into second row '''
 
 INSERT INTO Event(operationType)
 VALUES (value1, value2)
